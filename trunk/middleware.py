c327a03ac4eb24f946c70c838066ff00e601eb8c
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-04-27 00:34:18 +0000

    First project code.

---
+++ trunk/middleware.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings 
+try:
+    MOBILE_IGNORE_ADMIN = settings.MOBILE_IGNORE_ADMIN
+except AttributeError:
+    MOBILE_IGNORE_ADMIN = False
+
+
+class DjangoMobileMiddleware(object):
+    def process_response(self, request, response):
+        if MOBILE_IGNORE_ADMIN and request.user.is_authenticated() and request.user.is_staff:
+            return response
+        else:
+            content = response.content
+            index = content.upper().find('</BODY>')
+            if index == -1:
+                return response
+            else:
+                response.content =  content[:index] + '<h3>'+str(request.META['HTTP_USER_AGENT'])+'</h3>' + content[index:]
+                return response
+
+    def process_request(self, request):
+        return None
+
