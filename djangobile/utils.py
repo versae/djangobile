ff70d8298f84dfcf303c52bb3ac15b6c2dc258fc
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-04-29 17:44:31 +0000

    First version of device identifier with settings to override WURFL options.

---
+++ djangobile/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import re
+
+from django.conf import settings
+from django.utils.translation import gettext as _
+
+def get_device_family(user_agent):
+    user_agents_ignore_case = hasattr(settings, 'MOBILE_USER_AGENTS_IGNORE_CASE') and settings.MOBILE_USER_AGENTS_IGNORE_CASE
+    
+    if hasattr(settings, 'MOBILE_USER_AGENTS_PATTERNS') and isinstance(settings.MOBILE_USER_AGENTS_PATTERNS, (list, tuple)):
+        for user_agent_pattern in settings.MOBILE_USER_AGENTS_PATTERNS:
+            if isinstance(user_agent_pattern, (list, tuple)) and len(user_agent_pattern) == 2:
+                pattern = user_agent_pattern[0]
+                device_family = user_agent_pattern[1]
+                if not user_agents_ignore_case:
+                    if re.compile(pattern).match(user_agent):
+                        return device_family
+                    else:
+                        continue
+                else:
+                    if re.compile(pattern, re.I).match(user_agent.lower()):
+                        return device_family
+                    else:
+                        continue
+            else:
+                raise UserAgentPatternException
+    else:
+        if "firefox" in user_agent:
+            return "firefox"
+        elif "konqueror" in user_agent:
+            return "konqueror"
+        elif "nokia" in user_agent:
+            return "nokia"
+        else:
+            return None
+
+class UserAgentPatternException(Exception):
+    pass
