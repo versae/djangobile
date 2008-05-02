06c136c6f99b4e08e6cdff27db650e771adc7ec9
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-05-02 10:53:32 +0000

    Added WURFL_CLASS option in settings.

--- trunk/djangobile/utils.py
+++ trunk/djangobile/utils.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
-import re
 from os import path
 
 from django.conf import settings
 from django.utils.translation import gettext as _
 
-from wurfl import devices
 from pywurfl.algorithms import Tokenizer
 
 
 def get_device(user_agent=None, device_id=None):
     assert(((user_agent and not device_id) or (not user_agent and device_id)), _('user_agent or device_id must be passed, but not both.'))
+    if hasattr(settings, 'WURFL_CLASS'):
+        devices = getattr(__import__(settings.WURFL_CLASS, {}, {}, ['devices']), 'devices')
+    else:
+        from djangobile.wurfl import devices
+
     if user_agent:
         device = devices.select_ua(user_agent, filter_noise=True, search=Tokenizer(), instance=True)
     else:
         device = devices.select_id(user_agent, instance=True)
     device_dic = {}
-    for device_property in device:
-        capability = device_property[1]
-        value = device_property[2]
+    for group, capability, value in device:
         device_dic[capability] = value
     device_dic['id'] = device.devid
     device_dic['user_agent'] = device.devua
