ddda0c232443db2e5c8e054049c6cd891b4f8014
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-05-05 13:35:21 +0000

    Alpha version for GenericXhtml IDEAL processor. Minor fixes.

--- djangobile/ideal.py
+++ djangobile/ideal.py
@@ -1,9 +1,38 @@
 # -*- coding: utf-8 -*-
+import xml.dom.minidom
+from xml.dom.minidom import Node
 
 
 class Ideal(object):
     def __init__(self, source, origin=None, name=None):
         self.source = source
 
+    def render(self, context=None, cls=None):
+        if not context:
+            return self.source
+        device = context.get('device')
+        if cls:
+            processor = cls(self.source)
+            return processor.render(context)
+        fall_back = device.get('fall_back').lower()
+        if fall_back in ('generic', 'generic_xhtml'):
+            processor = GenericXhtml(self.source)
+        else:
+            return self.source
+        return processor.render(context)
+
+
+class GenericXhtml(object):
+    def __init__(self, source, namespace="http://morfeo-project.org/mymobileweb"):
+        self.source = source
+        self.doc = xml.dom.minidom.parseString(source)
+        self.document = doc.getElementsByTagNameNS(namespace, 'document')[0]
+
+    def ns(self, node):
+        return "%s:%s" % (self.document.prefix, node)
+
     def render(self, context):
         return self.source
+
+    def entryfield(self):
+        pass
--- djangobile/shortcuts.py
+++ djangobile/shortcuts.py
@@ -2,23 +2,10 @@
 from django.http import HttpResponse
 from django.template import TemplateDoesNotExist
 
-from djangobile.template import loader
+from djangobile.template.loader import render_to_string
 
 
 def render_to_response(*args, **kwargs):
     httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
-    content = render_to_ideal(*args, **kwargs)
+    content = render_to_string(*args, **kwargs)
     return HttpResponse(content, **httpresponse_kwargs)
-
-def render_to_ideal(template_name, dictionary=None, context_instance=None):
-    dictionary = dictionary or {}
-    device = context_instance.get('device', None)
-    if isinstance(template_name, (list, tuple)):
-        t = loader.select_template(template_name, device)
-    else:
-        t = loader.get_template(template_name, device)
-    if context_instance:
-        context_instance.update(dictionary)
-    else:
-        context_instance = Context(dictionary)
-    return t.render(context_instance)
