90b476fd0f46d368a0c0abe841419240a7721649
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-05-13 22:34:22 +0000

    Added new settings. First IDEAL implementation templates-based. Minor fixes.

--- djangobile/ideal.py
+++ djangobile/ideal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import xml.dom.minidom
-from xml.dom.minidom import Node
+from xml.dom import minidom
+
+from django.template.loader import get_template_from_string, get_template, render_to_string
 
 
 class Ideal(object):
@@ -25,14 +26,21 @@ class Ideal(object):
 class GenericXhtml(object):
     def __init__(self, source, namespace="http://morfeo-project.org/mymobileweb"):
         self.source = source
-        self.doc = xml.dom.minidom.parseString(source)
-        self.document = self.doc.getElementsByTagNameNS(namespace, 'document')[0]
+        self.ns = namespace
+        self.doc = minidom.parseString(source)
+        self.document = self.doc.getElementsByTagNameNS(self.ns, 'document')[0]
+        self.data = {}
+        self.__load_initial_data__()
+
+    def __load_initial_data__(self):
+        title = self.doc.getElementsByTagNameNS(self.ns, 'title')[0]
+        self.data['title'] = title.firstChild.data
 
     def ns(self, node):
         return "%s:%s" % (self.document.prefix, node)
 
     def render(self, context):
-        return self.source
+        return render_to_string('generic_xhtml.html', self.data, context)
 
     def entryfield(self):
         pass
