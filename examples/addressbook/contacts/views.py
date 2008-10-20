a59858352e1402232a03ebf86d4a585fb9e4af2d
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2008-10-20 21:51:10 +0000

    Added djangobile render_to_response shortcut to views. Fixed Fennec (Mozilla Mobile Firefox) device as extra device.

--- examples/addressbook/contacts/views.py
+++ examples/addressbook/contacts/views.py
@@ -2,9 +2,11 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import gettext as _
+
+from djangobile.shortcuts import render_to_response
 
 
 def redirect(request):
@@ -16,6 +18,7 @@ def show(request):
 
 
 def list(request):
+    return render_to_response("list.html", {}, context_instance=RequestContext(request))
     return HttpResponse('listado')
 
 
--- examples/addressbook/extra_devices.py
+++ examples/addressbook/extra_devices.py
@@ -1,7 +1,7 @@
 from djangobile import devices
 
 
-devices.add(parent='generic',
-            devid='fennec',
+devices.add(parent='mozilla_ver5',
+            devid='fennec_101a',
             devua='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1b2pre) Gecko/20081015 Fennec/1.0a1',
             actual_device_root=True)
