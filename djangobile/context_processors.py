233328d4df62235636b15e60b38021f2c6e0bde7
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2009-01-28 16:08:39 +0000

    Changed again... Am I going crazy?

--- djangobile/context_processors.py
+++ djangobile/context_processors.py
@@ -9,7 +9,7 @@ def mobile(request):
         if device.get('user_agent', False) != request.META['HTTP_USER_AGENT']:
             device = get_device(request.META['HTTP_USER_AGENT'])
             request.session['device'] = device
-            device_log(request, device)
+        device_log(request, device)
     elif request.session.test_cookie_worked():
         request.session.delete_test_cookie()
         device = request.session.get('device', False)
