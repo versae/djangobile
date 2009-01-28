f2fc8e793588b7c92da1d41ebe4f0932dbeab835
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2009-01-28 15:43:39 +0000

    Enhanced how and when djangobile shows the device log.

--- trunk/djangobile/context_processors.py
+++ trunk/djangobile/context_processors.py
@@ -9,17 +9,17 @@ def mobile(request):
         if device.get('user_agent', False) != request.META['HTTP_USER_AGENT']:
             device = get_device(request.META['HTTP_USER_AGENT'])
             request.session['device'] = device
-        device_log(request)
+            device_log(request, device)
     elif request.session.test_cookie_worked():
         request.session.delete_test_cookie()
         device = request.session.get('device', False)
         if not device:
             device = get_device(request.META['HTTP_USER_AGENT'])
             request.session['device'] = device
-            device_log(request)
+            device_log(request, device)
     else:
         device = get_device(request.META['HTTP_USER_AGENT'])
         request.session['device'] = device
-        device_log(request)
+        device_log(request, device)
     request.session.set_test_cookie()
     return {'device': device}
