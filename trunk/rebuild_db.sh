ef488dd3979c3bbf548bfbc1f50f6bbf418f3a96
versae <versae@852372e3-cf4b-0410-bc76-65afb2133a1c>
2009-05-25 15:56:28 +0000

    Updated default wurfl.py adding new patchs for web browsers.

--- trunk/rebuild_db.sh
+++ trunk/rebuild_db.sh
@@ -14,10 +14,18 @@ if (( $? )) ; then
 fi
 
 echo "Generating wurfl.py"
-xsltproc --stringparam 'file' "wurfl_patch_2.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl.xml" > "wurfl/wurfl_tmp.xml"
-xsltproc --stringparam 'file' "wurfl_patch_1.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl_tmp.xml" > "wurfl/wurfl_patched.xml"
+echo " - Applying patch wurfl_patch_2"
+xsltproc --stringparam 'file' "wurfl_patch_2.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl.xml" > "wurfl/wurfl_tmp_2.xml"
+echo " - Applying patch wurfl_patch_1"
+xsltproc --stringparam 'file' "wurfl_patch_1.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl_tmp_2.xml" > "wurfl/wurfl_tmp_1.xml"
+echo " - Applying patch wurfl_patch"
+xsltproc --stringparam 'file' "wurfl_patch.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl_tmp_1.xml" > "wurfl/wurfl_tmp.xml"
+echo " - Applying patch web_browsers_patch"
+xsltproc --stringparam 'file' "web_browsers_patch.xml" "wurfl/patch_wurfl.xsl" "wurfl/wurfl_tmp_1.xml" > "wurfl/wurfl_patched.xml"
 xsltproc "wurfl/check_wurfl.xsl" "wurfl/wurfl_patched.xml"
 rm wurfl/wurfl_tmp.xml
+rm wurfl/wurfl_tmp_1.xml
+rm wurfl/wurfl_tmp_2.xml
 python wurfl2python.py wurfl/wurfl_patched.xml -o djangobile/wurfl.py
 if (( $? )) ; then
   echo "Unable to create wurfl.py."
